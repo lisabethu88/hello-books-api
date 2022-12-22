c1b3c5f978766768071d0f0a180b83cf5003bd01
Lisa Utsett <lisabethutsett@gmail.com>
2022-12-21 19:24:32 -0500

    Created wrapper functions for endpoints and placed routes in a separate file

--- app/routes.py
+++ app/routes.py
@@ -1,113 +1,26 @@
-from app import db
-from app.models.book import Book
-from flask import Blueprint, jsonify, abort, make_response, request
-
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, "Spin", "A scifi novel set in future Earth."),
-#     Book(2, "Where the Wild Things Are", "An illustrated children's book about monsters."),
-#     Book(3, "It", "A horror novel about a sadistic clown who terrorizes the residents of Derry, Maine.")
-# ] 
-
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
-
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-
-#     abort(make_response({"message":f"book {book_id} not found"}, 404))
-# 
-# books_bp = Blueprint("books", __name__, url_prefix="/books")
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response)
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-#     return {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description,
-#                 }
-
-
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
-
-    book = Book.query.get(book_id)
-    if not book:
-        abort(make_response({"message":f"book {book_id} not found"}, 404))
-
-    return book
+from flask import Blueprint
+from app.books import *
             
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 @books_bp.route("", methods=["POST"])
 def create_book():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
-
-    db.session.add(new_book)
-    db.session.commit()
-
-    return make_response(f"Book {new_book.title} successfully created", 201)
+    create_book_implementation()
 
 @books_bp.route("", methods=["GET"])
 def read_all_books():
-    books = Book.query.all()
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
+    read_all_books_implementation()
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    book = validate_book(book_id)
-    return {
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-            }
+    handle_book_implementation(book_id)
 
 @books_bp.route("/<book_id>", methods=["PUT"])
 def update_book(book_id):
-    book = validate_book(book_id)
-    request_body = request.get_json()
-    book.title = request_body["title"]
-    book.description = request_body["description"]
-    db.session.commit()
-    return make_response(f"Book #{book.id} successfully updated")
+    update_book_implementation(book_id)
 
 @books_bp.route("/<book_id>", methods=["DELETE"])
 def delete_book(book_id):
-    book = validate_book(book_id)
-    db.session.delete(book)
-    db.session.commit()
-    return make_response(f"Book #{book.id} successfully deleted")
+    delete_book_implementation(book_id)
 
 
 
